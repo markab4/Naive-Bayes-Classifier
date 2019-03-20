a15aa2fb15dd7ea7e90b263589ea39b02f464a33
Mark Abramov <markabramov01@gmail.com>
2019-03-19 20:19:11 -0400

    loop through files in the pos and neg directories

--- NB.py
+++ NB.py
@@ -4,3 +4,4 @@
 #   the file where the parameters of the resulting model will be saved, and
 #   the output file where you will write predictions made by the classifier on the test data (one example per line).
 # The last line in the output file should list the overall accuracy of the classifier on the test data.
+
--- pre-process.py
+++ pre-process.py
@@ -5,16 +5,31 @@
 #   perform pre-processing on each file
 #   output the files in the vector format to be used by NB.py.
 
+# Prior to building feature vectors:
+#   separate punctuation from words
+#   lowercase the words in the reviews.
+#   use add-one smoothing for BOW features
+
 # The training and the test files should have the following format:
 #   one example per line
 #   each line corresponds to an example
 #   first column is the label
 #   the other columns are feature values.
 
-# Prior to building feature vectors:
-#   separate punctuation from words
-#   lowercase the words in the reviews.
-#   use add-one smoothing for BOW features
+# Save the parameters of BOW model in a file called movie-review-BOW.NB .
 
-# Save the parameters of BOW model in a file called movie-review-BOW.NB.
+import os
+import sys
+import re
 
+directory = sys.argv[1]
+pos_reviews = os.path.join(directory, "pos")
+neg_reviews = os.path.join(directory, "neg")
+
+for folder in [pos_reviews, neg_reviews]:
+    for filename in os.listdir(folder):
+        if filename.endswith(".txt"):
+            f = open(os.path.join(folder, filename), "r")
+            text = f.read()
+            review = re.findall(r"[\w']+|[.,!?;:]", text)
+            print(text)
